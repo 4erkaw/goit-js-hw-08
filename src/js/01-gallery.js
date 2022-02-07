84ae56d3757bc103e2221ceb20c9bef629d8cf0a
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-02-07 14:45:19 +0200

    1 task start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,57 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  )
+  .join(" ");
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+gallery.addEventListener("click", onGalleryElemClick);
+
+let instance = null;
+
+function onGalleryElemClick(e) {
+  e.preventDefault();
+  const origImg = e.target.dataset.source;
+
+  instance = basicLightbox.create(
+    `<img src="${origImg}" width = "800" height = "600">`,
+    {
+      onShow: addListener,
+      onClose: removeListener
+    }
+  );
+
+  instance.show();
+}
+
+function addListener() {
+    document.addEventListener("keydown", key)
+}
+
+
+function removeListener() {
+    document.removeEventListener("keydown", key)
+}
+
+function key(e){
+   if(e.code === "Escape"){
+       instance.close()
+   }
+}
