a36c2c20a404190e780674d3dbfb2115c0acffa7
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-02-08 18:46:18 +0200

    task 2-3 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+const STORAGE_CRNT_TIME = "videoplayer-current-time"
+
+player.on('timeupdate', throttle(currentTime, 1000))
+
+function currentTime(data){
+    localStorage.setItem(STORAGE_CRNT_TIME, data.seconds)
+}
+
+const time = localStorage.getItem(STORAGE_CRNT_TIME) || 0
+
+player.setCurrentTime(time)
+
+
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import * as storage from '../services/localStorage';
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector(".feedback-form")
+}
+const STORAGE_KEY = "feedback-form-state";
+
+function onFormInput(e){
+    const {name, value} = e.target
+
+    const parsedData = storage.get(STORAGE_KEY) ?? {};
+    const formData = {
+        ...parsedData,
+        [name]:value
+    }
+    storage.save(STORAGE_KEY, formData);
+}
+
+const reuseData = () => {
+    const parsedData = storage.get(STORAGE_KEY) ?? {};
+    const inputNames = Object.keys(parsedData);
+
+  inputNames.forEach(inputName => {
+    const input = refs.form.elements[inputName];
+    input.value = parsedData[inputName];
+});
+}
+
+reuseData()
+
+const onSumbitClick = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const finalData = {};
+  
+    for (const [key, value] of formData.entries()) {
+      if (!value) {
+        alert('Please fill in all the fields!');
+        return;
+      }
+      finalData[key] = value;
+    }
+  
+    console.log(finalData);
+    form.reset();
+    storage.remove(STORAGE_KEY);
+  };
+
+
+refs.form.addEventListener('input', throttle((onFormInput),500))
+refs.form.addEventListener('submit', onSumbitClick)
 
